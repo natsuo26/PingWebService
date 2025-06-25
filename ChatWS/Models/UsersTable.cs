b4ae667a5e4697c1126d9f615c73bbfe072fc4ed
Ajr1993 <alexrichards10@outlook.com>
2025-06-25 18:34:13 +0100

    Added my API files

---
+++ ChatWS/Models/UsersTable.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chat_App_API.Models
+{
+    public class UsersTable
+    {
+        [Key]
+        public int userId { get; set; }
+
+        public string userName { get; set; } = string.Empty;
+
+        public string password { get; set; } = string.Empty;
+        
+    }
+}
